b96891a23d8235baac9d6817d8d105ce9dca9681
Nikita Grechanikov <ngrechanikov@gmail.com>
2025-04-01 22:48:20 +0300

    added wav2lip interface class which needs to get fixed. inference works fine with inference.py

--- Wav2Lip/models/conv.py
+++ Wav2Lip/models/conv.py
@@ -19,6 +19,21 @@ class Conv2d(nn.Module):
         return self.act(out)
 
 
+class nonorm_Conv2d(nn.Module):
+    def __init__(
+        self, cin, cout, kernel_size, stride, padding, residual=False, *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.conv_block = nn.Sequential(
+            nn.Conv2d(cin, cout, kernel_size, stride, padding),
+        )
+        self.act = nn.LeakyReLU(0.01, inplace=True)
+
+    def forward(self, x):
+        out = self.conv_block(x)
+        return self.act(out)
+
+
 class Conv2dTranspose(nn.Module):
     def __init__(
         self, cin, cout, kernel_size, stride, padding, output_padding=0, *args, **kwargs
