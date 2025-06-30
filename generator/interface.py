e05d3a1d83dc35f8d64d22b54f33d12927235bf8
Nick <ngrechanikov@gmail.com>
2025-06-30 12:01:43 +0300

    added GeneratorInterface template

---
+++ generator/interface.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class GeneratorInterface:
+    def __init__(self, video_paths: List[str], text_path: str):
+        self.video_path = video_paths
+        self.text_path = text_path
+
+    def generate_mp3(self):
+        pass
+
+    def generate_mp4(self):
+        pass
