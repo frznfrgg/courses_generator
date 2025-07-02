4f143605b97941ea3bb8c0b5788748bcd932250c
Nick <ngrechanikov@gmail.com>
2025-07-02 15:12:22 +0300

    Added wav2lip to interface, gen_results folder removed

--- generator/gen_results/README.md
+++
@@ -1,1 +0,0 @@
-In this folder the results of generation (audio and video files) will be stored
--- generator/interface.py
+++ generator/interface.py
@@ -1,23 +1,27 @@
+import os
 import random
 import uuid
 from typing import List
+
 from moviepy import AudioFileClip, VideoFileClip, concatenate_videoclips
 from mutagen.mp3 import MP3
+
 import xtts_ru.xtts_inference
-import os
+from Wav2Lip.interface import Wav2LipInterface
+
 
 class GeneratorInterface:
     def __init__(self, video_paths: List[str], text_path: str):
         self.video_paths = video_paths
         self.text_path = text_path
-    
+
     def _convert_vid2aud(self):
         video = VideoFileClip(self.video_paths[0])
-        wav_path = self.video_paths[0][:self.video_paths[0].rfind(".")] + ".wav"
+        wav_path = self.video_paths[0][: self.video_paths[0].rfind(".")] + ".wav"
         video.audio.write_audiofile(wav_path)
         return wav_path
-    
-    def generate_mp3(self):
+
+    def generate_raw_mp3(self):
         wav_path = self._convert_vid2aud()
         with open(self.text_path) as f:
             text = f.readlines()
@@ -26,12 +30,12 @@ class GeneratorInterface:
         xtts = xtts_ru.xtts_inference.XttsInference()
         transc, audio = xtts(clean_text, wav_path)
 
-        self.audio_path = audio
+        self.mp3_path = audio
         os.remove(wav_path)
 
-    def generate_mp4(self):
+    def generate_raw_mp4(self):
         # load generated audio and videoclips, extrafct durations
-        audio = MP3(self.audio_path)
+        audio = MP3(self.mp3_path)
         target_duration = audio.info.length
         all_clips = [VideoFileClip(p) for p in self.video_paths]
 
@@ -54,14 +58,11 @@ class GeneratorInterface:
 
         # save results results
         final_clip = concatenate_videoclips(clips, method="compose")
-        audio_clip = AudioFileClip(self.audio_path)
-        final_clip = final_clip.with_audio(audio_clip)
-        result_path = str(uuid.uuid4()) + ".mp4"
-        print(result_path)
-        final_clip.write_videofile(result_path)
-    
-    def generate(self):
-        self.generate_mp3()
-        self.generate_mp4()
+        self.mp4_path = str(uuid.uuid4()) + ".mp4"
+        final_clip.write_videofile(self.mp4_path)
 
-
+    def generate_with_wav2lip(self):
+        self.generate_raw_mp3()
+        self.generate_raw_mp4()
+        wav2lip = Wav2LipInterface(video_path=self.mp4_path, audio_path=self.mp3_path)
+        wav2lip.generate()
